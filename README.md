5ab18a1827a2f26c89b5143ccc5cbecc0c722671
kylegundersen <kylegundersen@me.com>
2022-04-22 07:41:29 -0500

    added more info to readme and a simple router implementation using the typescript version

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # DOMinationJS-Samples
 These are common implementation examples provided to help people use this awesome minimalistic library.
+
+## Included are three implementation:
+**Inline JS** - Single HTML page with an inline script tag.
+**TypeScript** - A TypeScript project with UMD webpack build process. (The only one with route example for now)
+**Vanilla JS** - An external JavaScipt file being referenced by a script tag.
+
+## Getting Started
+1. ```npm run init``` in package.json
+2. ```npm run init``` in typescript/package.json
+3. ```npm run build``` in typescript/package.json
+4. ```npm run start``` in package.json
+
+Once all these npm script are run it will open a new browser tab on localhost 3000. Feel free to play around, also included is a ```npm run watch``` command so the typescript will auto-build when changes are saved. So you can experiment and get quick feedback.
+
+
+## More to come..
+- Covering more advanced performance focused implementations for enterprise level applications.
--- typescript/src/index.ts
+++ typescript/src/index.ts
@@ -7,39 +7,70 @@ let backButton = DOM.create('button', { class: "header", text: "Back to index" }
     }
 }); 
 let header = DOM.create('div', { class: "header", style: "padding:20px;width:100%;background:#cFF", text: backButton });
-let newElement = DOM.create('h1', { class: "title", text: "TypeScript Sample" });
+
+/**
+ * Backing building and triggering events
+ * 
+ */
+let newElement = DOM.create('h1', { class: "title", text: "TypeScript Event Sample" });
 let newDesc = DOM.create("p", { text: "Hello, click the button to see how events work." });
 let newButton = DOM.create("button", { class: "class-1 class-2", id:'unique-guid', text: 'Click button to test' }, { 
     click : () => {
-        container.append(DOM.create("p", { text: "Button Clicked!", style: "padding:5px 20px;" }));
+        eventSectionContainer.append(DOM.create("p", { text: "Button Clicked!", style: "padding:5px 20px;" }));
     } });
-let container = DOM.create('div', { style: "padding:20px;" });
-container.append( newElement, newDesc, newButton );
-document.body.append(header, container);
-document.body.style.margin = "0";
+let eventSectionContainer = DOM.create('div', { style: "padding:20px;", append: [newElement, newDesc, newButton] });
 
-/* Routing pattern */
-let routerButton1 = DOM.create("button", { "goto-route":"/typescript/index.html", text: "Goto Route /typescript/index.html", style: "margin-left:10px" });
-let routerButton2 = DOM.create("button", { "goto-route":"/some/path", text: "Goto Route /some/path", style: "margin-left:10px" });
-let routeContainer1 = DOM.create('div', { style: "padding:20px;background:#f0e8ca", route: "/some/path", text: `Show when on "/some/path" route` });
-container.append(routerButton1, routerButton2, routeContainer1);
+
+/**
+ * Routing pattern
+ * 
+ */
+
+// Routing - Dom Build
+let routerTitle = DOM.create('h1', { class: "title", text: "Basic Routing Sample" });
+let routerButton1 = DOM.create("button", { routeLink: "/typescript/index.html", text: "Goto Route /typescript/index.html", style: "margin-left:10px" });
+let routerButton2 = DOM.create("button", { routeLink: "/some/path", text: "Goto Route /some/path", style: "margin-left:10px" });
+let routeContainer1 = DOM.create('div', { style: "padding:20px;background:#f0e8ca;margin-top:10px", routeShow: "/some/path", text: `Show when on "/some/path" route` });
+let routeContainer2 = DOM.create('div', { style: "padding:20px;background:#cae3f0;margin-top:10px", routeShow: "/typescript/index.html", text: `Show when on "/typescript/index.html" route` });
+let routerSectionContainer = DOM.create('div', { style: "padding:20px;", append: [routerTitle, routerButton1, routerButton2, routeContainer1, routeContainer2] });
 
 // Delegate SPA links
-DOM.addEventDelegate('click', '[goto-route]', (e: PointerEvent) => {
+DOM.addEventDelegate('click', '[routeLink]', (e: PointerEvent) => {
     let element : HTMLElement = e.target as HTMLElement;
-    DOM.setRoute(element.getAttribute('goto-route'));
-    render();
+    DOM.setRoute(element.getAttribute('routeLink'));
+    render(); // Rerender view Elements
 });
 
-render();
-function render(){
-    let routeControlledViews = DOM.selectAll('[route]');
+// Delegate SPA Routes
+function renderRouteViews(){
     let currentRoute = DOM.getRoute();
-    routeControlledViews.forEach((element:HTMLElement) => {
-       if(currentRoute.includes(element.getAttribute('route'))){ 
-           element.style.display = 'flex';
-       } else {
-            element.style.display = 'none';
-       }
+    // Adding the routeShow attribute on an html element will show it on a matching route
+    let routeViews = DOM.selectAll('[routeShow]');
+    routeViews.forEach((element:HTMLElement) => {
+        element.style.display = currentRoute.includes(element.getAttribute('routeShow')) ? 'flex' : 'none';
+    });
+    // Adding the routeHide attribute on an html element will hide it on a matching route
+    routeViews = DOM.selectAll('[routeHide]');
+    routeViews.forEach((element:HTMLElement) => {
+        element.style.display = currentRoute.includes(element.getAttribute('routeHide')) ? 'none' : 'flex';
     });
 }
+
+
+/**
+ *  Render Pipeline this will be triggered in the following ways
+ *  - Once on every first load
+ *  - Once on every internal page route event by click on a [routeLink] 
+ */
+function render(){
+    renderRouteViews();
+}
+
+// Add constructed elements to the page
+document.body.append(
+    header, 
+    eventSectionContainer,
+    routerSectionContainer
+    );
+document.body.style.margin = "0";
+render(); // Fire it once to render intial view
